34f55b2220284a0fd1d3febdcb0bdcf738fd2ebe
Woojin Lim <isp_lwj@naver.com>
2017-12-13 10:57:17 +0900

    Add new files

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace RSA
+{
+    partial class RSA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RSA));
+            this.First_Prime_Label = new System.Windows.Forms.Label();
+            this.Second_Prime_Label = new System.Windows.Forms.Label();
+            this.Encryption_Key_Label = new System.Windows.Forms.Label();
+            this.Decryption_Key_Label = new System.Windows.Forms.Label();
+            this.N_Label = new System.Windows.Forms.Label();
+            this.PI_N_Label = new System.Windows.Forms.Label();
+            this.P_Textbox = new System.Windows.Forms.TextBox();
+            this.Q_Textbox = new System.Windows.Forms.TextBox();
+            this.Encryption_Key_Textbox = new System.Windows.Forms.TextBox();
+            this.Decryption_Key_Textbox = new System.Windows.Forms.TextBox();
+            this.Plain_Label = new System.Windows.Forms.Label();
+            this.Cipher_Label = new System.Windows.Forms.Label();
+            this.Plain_Textbox = new System.Windows.Forms.TextBox();
+            this.Cipher_Textbox = new System.Windows.Forms.TextBox();
+            this.Random_Button = new System.Windows.Forms.Button();
+            this.Encrypt_Button = new System.Windows.Forms.Button();
+            this.Decrypt_Buttion = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.N_Textbox = new System.Windows.Forms.TextBox();
+            this.PI_N_Textbox = new System.Windows.Forms.TextBox();
+            this.Status_Textbox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // First_Prime_Label
+            // 
+            this.First_Prime_Label.AutoSize = true;
+            this.First_Prime_Label.Location = new System.Drawing.Point(12, 9);
+            this.First_Prime_Label.Name = "First_Prime_Label";
+            this.First_Prime_Label.Size = new System.Drawing.Size(13, 13);
+            this.First_Prime_Label.TabIndex = 0;
+            this.First_Prime_Label.Text = "p";
+            // 
+            // Second_Prime_Label
+            // 
+            this.Second_Prime_Label.AutoSize = true;
+            this.Second_Prime_Label.Location = new System.Drawing.Point(12, 36);
+            this.Second_Prime_Label.Name = "Second_Prime_Label";
+            this.Second_Prime_Label.Size = new System.Drawing.Size(13, 13);
+            this.Second_Prime_Label.TabIndex = 1;
+            this.Second_Prime_Label.Text = "q";
+            // 
+            // Encryption_Key_Label
+            // 
+            this.Encryption_Key_Label.AutoSize = true;
+            this.Encryption_Key_Label.Location = new System.Drawing.Point(12, 110);
+            this.Encryption_Key_Label.Name = "Encryption_Key_Label";
+            this.Encryption_Key_Label.Size = new System.Drawing.Size(13, 13);
+            this.Encryption_Key_Label.TabIndex = 2;
+            this.Encryption_Key_Label.Text = "e";
+            // 
+            // Decryption_Key_Label
+            // 
+            this.Decryption_Key_Label.AutoSize = true;
+            this.Decryption_Key_Label.Location = new System.Drawing.Point(12, 136);
+            this.Decryption_Key_Label.Name = "Decryption_Key_Label";
+            this.Decryption_Key_Label.Size = new System.Drawing.Size(13, 13);
+            this.Decryption_Key_Label.TabIndex = 3;
+            this.Decryption_Key_Label.Text = "d";
+            // 
+            // N_Label
+            // 
+            this.N_Label.AutoSize = true;
+            this.N_Label.Location = new System.Drawing.Point(11, 63);
+            this.N_Label.Name = "N_Label";
+            this.N_Label.Size = new System.Drawing.Size(21, 13);
+            this.N_Label.TabIndex = 4;
+            this.N_Label.Text = "N :";
+            // 
+            // PI_N_Label
+            // 
+            this.PI_N_Label.AutoSize = true;
+            this.PI_N_Label.Location = new System.Drawing.Point(12, 85);
+            this.PI_N_Label.Name = "PI_N_Label";
+            this.PI_N_Label.Size = new System.Drawing.Size(32, 13);
+            this.PI_N_Label.TabIndex = 5;
+            this.PI_N_Label.Text = "𝜑(n) :";
+            // 
+            // P_Textbox
+            // 
+            this.P_Textbox.Location = new System.Drawing.Point(31, 6);
+            this.P_Textbox.Name = "P_Textbox";
+            this.P_Textbox.Size = new System.Drawing.Size(330, 20);
+            this.P_Textbox.TabIndex = 0;
+            // 
+            // Q_Textbox
+            // 
+            this.Q_Textbox.Location = new System.Drawing.Point(31, 32);
+            this.Q_Textbox.Name = "Q_Textbox";
+            this.Q_Textbox.Size = new System.Drawing.Size(330, 20);
+            this.Q_Textbox.TabIndex = 2;
+            // 
+            // Encryption_Key_Textbox
+            // 
+            this.Encryption_Key_Textbox.Location = new System.Drawing.Point(31, 107);
+            this.Encryption_Key_Textbox.Name = "Encryption_Key_Textbox";
+            this.Encryption_Key_Textbox.Size = new System.Drawing.Size(330, 20);
+            this.Encryption_Key_Textbox.TabIndex = 5;
+            // 
+            // Decryption_Key_Textbox
+            // 
+            this.Decryption_Key_Textbox.Location = new System.Drawing.Point(31, 133);
+            this.Decryption_Key_Textbox.Name = "Decryption_Key_Textbox";
+            this.Decryption_Key_Textbox.Size = new System.Drawing.Size(330, 20);
+            this.Decryption_Key_Textbox.TabIndex = 6;
+            // 
+            // Plain_Label
+            // 
+            this.Plain_Label.AutoSize = true;
+            this.Plain_Label.Location = new System.Drawing.Point(11, 165);
+            this.Plain_Label.Name = "Plain_Label";
+            this.Plain_Label.Size = new System.Drawing.Size(30, 13);
+            this.Plain_Label.TabIndex = 10;
+            this.Plain_Label.Text = "Plain";
+            // 
+            // Cipher_Label
+            // 
+            this.Cipher_Label.AutoSize = true;
+            this.Cipher_Label.Location = new System.Drawing.Point(11, 190);
+            this.Cipher_Label.Name = "Cipher_Label";
+            this.Cipher_Label.Size = new System.Drawing.Size(37, 13);
+            this.Cipher_Label.TabIndex = 11;
+            this.Cipher_Label.Text = "Cipher";
+            // 
+            // Plain_Textbox
+            // 
+            this.Plain_Textbox.Location = new System.Drawing.Point(50, 162);
+            this.Plain_Textbox.Name = "Plain_Textbox";
+            this.Plain_Textbox.Size = new System.Drawing.Size(478, 20);
+            this.Plain_Textbox.TabIndex = 7;
+            // 
+            // Cipher_Textbox
+            // 
+            this.Cipher_Textbox.Location = new System.Drawing.Point(50, 187);
+            this.Cipher_Textbox.Name = "Cipher_Textbox";
+            this.Cipher_Textbox.Size = new System.Drawing.Size(478, 20);
+            this.Cipher_Textbox.TabIndex = 9;
+            // 
+            // Random_Button
+            // 
+            this.Random_Button.Location = new System.Drawing.Point(367, 4);
+            this.Random_Button.Name = "Random_Button";
+            this.Random_Button.Size = new System.Drawing.Size(75, 23);
+            this.Random_Button.TabIndex = 1;
+            this.Random_Button.Text = "Random";
+            this.Random_Button.UseVisualStyleBackColor = true;
+            this.Random_Button.Click += new System.EventHandler(this.Random_Button_Click);
+            // 
+            // Encrypt_Button
+            // 
+            this.Encrypt_Button.Location = new System.Drawing.Point(534, 160);
+            this.Encrypt_Button.Name = "Encrypt_Button";
+            this.Encrypt_Button.Size = new System.Drawing.Size(75, 23);
+            this.Encrypt_Button.TabIndex = 8;
+            this.Encrypt_Button.Text = "Encrypt!";
+            this.Encrypt_Button.UseVisualStyleBackColor = true;
+            this.Encrypt_Button.Click += new System.EventHandler(this.Encrypt_Button_Click);
+            // 
+            // Decrypt_Buttion
+            // 
+            this.Decrypt_Buttion.Location = new System.Drawing.Point(534, 187);
+            this.Decrypt_Buttion.Name = "Decrypt_Buttion";
+            this.Decrypt_Buttion.Size = new System.Drawing.Size(75, 23);
+            this.Decrypt_Buttion.TabIndex = 11;
+            this.Decrypt_Buttion.Text = "Decrypt!";
+            this.Decrypt_Buttion.UseVisualStyleBackColor = true;
+            this.Decrypt_Buttion.Click += new System.EventHandler(this.Decrypt_Buttion_Click);
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(11, 254);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(37, 13);
+            this.Status.TabIndex = 17;
+            this.Status.Text = "Status";
+            // 
+            // N_Textbox
+            // 
+            this.N_Textbox.Location = new System.Drawing.Point(31, 58);
+            this.N_Textbox.Name = "N_Textbox";
+            this.N_Textbox.Size = new System.Drawing.Size(330, 20);
+            this.N_Textbox.TabIndex = 3;
+            // 
+            // PI_N_Textbox
+            // 
+            this.PI_N_Textbox.Location = new System.Drawing.Point(50, 82);
+            this.PI_N_Textbox.Name = "PI_N_Textbox";
+            this.PI_N_Textbox.Size = new System.Drawing.Size(311, 20);
+            this.PI_N_Textbox.TabIndex = 4;
+            // 
+            // Status_Textbox
+            // 
+            this.Status_Textbox.Location = new System.Drawing.Point(12, 270);
+            this.Status_Textbox.Name = "Status_Textbox";
+            this.Status_Textbox.ReadOnly = true;
+            this.Status_Textbox.Size = new System.Drawing.Size(608, 235);
+            this.Status_Textbox.TabIndex = 20;
+            this.Status_Textbox.Text = "";
+            // 
+            // RSA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 517);
+            this.Controls.Add(this.Status_Textbox);
+            this.Controls.Add(this.PI_N_Textbox);
+            this.Controls.Add(this.N_Textbox);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Decrypt_Buttion);
+            this.Controls.Add(this.Encrypt_Button);
+            this.Controls.Add(this.Random_Button);
+            this.Controls.Add(this.Cipher_Textbox);
+            this.Controls.Add(this.Plain_Textbox);
+            this.Controls.Add(this.Cipher_Label);
+            this.Controls.Add(this.Plain_Label);
+            this.Controls.Add(this.Decryption_Key_Textbox);
+            this.Controls.Add(this.Encryption_Key_Textbox);
+            this.Controls.Add(this.Q_Textbox);
+            this.Controls.Add(this.P_Textbox);
+            this.Controls.Add(this.PI_N_Label);
+            this.Controls.Add(this.N_Label);
+            this.Controls.Add(this.Decryption_Key_Label);
+            this.Controls.Add(this.Encryption_Key_Label);
+            this.Controls.Add(this.Second_Prime_Label);
+            this.Controls.Add(this.First_Prime_Label);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RSA";
+            this.Text = "RSA Encryption";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label First_Prime_Label;
+        private System.Windows.Forms.Label Second_Prime_Label;
+        private System.Windows.Forms.Label Encryption_Key_Label;
+        private System.Windows.Forms.Label Decryption_Key_Label;
+        private System.Windows.Forms.Label N_Label;
+        private System.Windows.Forms.Label PI_N_Label;
+        private System.Windows.Forms.TextBox P_Textbox;
+        private System.Windows.Forms.TextBox Q_Textbox;
+        private System.Windows.Forms.TextBox Encryption_Key_Textbox;
+        private System.Windows.Forms.TextBox Decryption_Key_Textbox;
+        private System.Windows.Forms.Label Plain_Label;
+        private System.Windows.Forms.Label Cipher_Label;
+        private System.Windows.Forms.TextBox Plain_Textbox;
+        private System.Windows.Forms.TextBox Cipher_Textbox;
+        private System.Windows.Forms.Button Random_Button;
+        private System.Windows.Forms.Button Encrypt_Button;
+        private System.Windows.Forms.Button Decrypt_Buttion;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.TextBox N_Textbox;
+        private System.Windows.Forms.TextBox PI_N_Textbox;
+        private System.Windows.Forms.RichTextBox Status_Textbox;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,427 @@
+﻿/*************************************************************************
+> Project Name: RSA
+> Author: Woojin Lim
+> Purpose: Functions to encrypt with RSA algorithm.
+> Created Time: 2017/12/12
+> Copyright (c) 2017, Woojin Lim
+*************************************************************************/
+
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA
+{
+    public partial class RSA : Form
+    {
+        public void AppendText(string text, Color color)
+        {
+            Status_Textbox.SelectionStart = Status_Textbox.TextLength;
+            Status_Textbox.SelectionLength = 0;
+
+            Status_Textbox.SelectionColor = color;
+            Status_Textbox.AppendText(text);
+            Status_Textbox.SelectionColor = Status_Textbox.ForeColor;
+        }
+        ulong GetPowResult(ulong baseValue, ulong exp, ulong mod)
+        {
+            baseValue %= mod;
+            ulong result = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                {
+                    result = (result * baseValue) % mod;
+                }
+                baseValue = (baseValue * baseValue) % mod;
+                exp >>= 1;
+            }
+            return result;
+        }
+        //Thanks to https://stackoverflow.com/a/8498251
+
+        public ulong GetRandomPrime(ulong exception)
+        {
+            Random rand = new Random();
+            ulong random = 0;
+            do
+            {
+                random = Convert.ToUInt64(rand.Next(2, 10000));
+                random += (random % 2 == 0 ? Convert.ToUInt64(1) : Convert.ToUInt64(0));
+            } while (random == exception || !IsPrime(random));
+            return random;
+        }
+
+        public ulong GetGcd(ulong firstValue, ulong secondValue)
+        {
+            ulong r;
+            while (secondValue != 0)
+            {
+                r = firstValue % secondValue;
+                firstValue = secondValue;
+                secondValue = r;
+            }
+            return firstValue;
+        }
+
+        public bool IsPrime(ulong value)
+        {
+            ulong i = 0;
+            ulong sqrt = (ulong)Math.Sqrt(value);
+            for (i = 2; i < sqrt; ++i)
+            {
+                if (value % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public ulong GetEValue(ulong n, ulong pi_n)
+        {
+            Random rand = new Random();
+            ulong random = Convert.ToUInt64(rand.Next(2, Convert.ToInt32(pi_n)));
+            while (GetGcd(random, pi_n) != 1 || random == n)
+            {
+                if (random > pi_n)
+                {
+                    random = Convert.ToUInt64(rand.Next(2, Convert.ToInt32(pi_n)));
+                }
+                ++random;
+            }
+            return random;
+        }
+
+        public ulong GetDValue(ulong e, ulong pi_n)
+        {
+            Random rand = new Random();
+            ulong random = Convert.ToUInt64(rand.Next(2, Convert.ToInt32(pi_n)));
+            while ((e * random) % pi_n != 1)
+            {
+                if (random > pi_n)
+                {
+                    random = Convert.ToUInt64(rand.Next(2, Convert.ToInt32(pi_n)));
+                }
+                ++random;
+            }
+            return random;
+        }
+
+        public RSA()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Random_Button_Click(object sender, EventArgs e)
+        {
+            //Basic Variable Declaration
+            ulong p_value = 0;
+            ulong q_value = 0;
+            ulong n_value = 0;
+            ulong pi_n_value = 0;
+            ulong e_value = 0;
+            ulong d_value = 0;
+
+            Status_Textbox.Text = "";
+
+            //Get Random Prime for p
+            p_value = GetRandomPrime(1);
+            P_Textbox.Text = p_value.ToString();
+
+            //Get Random Prime for q        ( p != q )
+            q_value = GetRandomPrime(p_value);
+            Q_Textbox.Text = q_value.ToString();
+
+            //n == p * q
+            n_value = p_value * q_value;
+            N_Textbox.Text = n_value.ToString();
+
+            //𝜑(n) == (p-1) * (q-1)
+            pi_n_value = (p_value - 1) * (q_value - 1);
+            PI_N_Textbox.Text = pi_n_value.ToString();
+
+            //e is random number.           ( GCD(𝜑(n), e) == 1 && e =! n )
+            e_value = GetEValue(p_value, pi_n_value);
+            Encryption_Key_Textbox.Text = e_value.ToString();
+
+            //d is random number.           ( (e * d) % pi_n == 1 )
+            d_value = GetDValue(e_value, pi_n_value);
+            Decryption_Key_Textbox.Text = d_value.ToString();
+
+            AppendText("Random number created!!!\n", Color.Black);
+        }
+
+        private void Encrypt_Button_Click(object sender, EventArgs e)
+        {
+            //Basic Variable Declaration
+
+            String temp = "";
+
+            ulong p_value = 0;
+            ulong q_value = 0;
+            ulong n_value = 0;
+            ulong pi_n_value = 0;
+            ulong e_value = 0;
+            ulong d_value = 0;
+
+            String plain = Plain_Textbox.Text;
+            String cipher = "";
+
+            int error = 0;
+            int len = plain.Length;
+
+            Status_Textbox.Text = "";
+
+            //Start checking error
+
+            //Check if any values are empty
+            if (P_Textbox.Text == "")
+            {
+                AppendText("p value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (Q_Textbox.Text == "")
+            {
+                AppendText("q value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (N_Textbox.Text == "")
+            {
+                AppendText("n value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (PI_N_Textbox.Text == "")
+            {
+                AppendText("𝜑(n) value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (Encryption_Key_Textbox.Text == "")
+            {
+                AppendText("e value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (Decryption_Key_Textbox.Text == "")
+            {
+                AppendText("d value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (Plain_Textbox.Text == "")
+            {
+                AppendText("Plain text is empty!!!\n", Color.Red);
+                ++error;
+            }
+
+            if (error != 0)
+            {
+                return;
+            }
+
+            //Check if values are not a number
+            try
+            {
+                p_value = Convert.ToUInt64(P_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("p value is not a number!!!\n", Color.Red);
+                ++error;
+            }
+            try
+            {
+                q_value = Convert.ToUInt64(Q_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("q value is not a number!!!\n", Color.Red);
+                ++error;
+            }
+            try
+            {
+                n_value = Convert.ToUInt64(N_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("n value is not a number!!!\n", Color.Red);
+                ++error;
+            }
+            try
+            {
+                pi_n_value = Convert.ToUInt64(PI_N_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("𝜑(n) value is not a number!!!\n", Color.Red);
+                ++error;
+            }
+            try
+            {
+                e_value = Convert.ToUInt64(Encryption_Key_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("e value is not a number!!!\n", Color.Red);
+                ++error;
+            }
+            try
+            {
+                d_value = Convert.ToUInt64(Decryption_Key_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("d value is not a number!!!\n", Color.Red);
+                ++error;
+            }
+            if (error != 0)
+            {
+                return;
+            }
+
+            //Check if values are wrong
+            if (!IsPrime(p_value))
+            {
+                AppendText("P value is not Prime!!!\n", Color.Red);
+                ++error;
+            }
+            if (!IsPrime(q_value))
+            {
+                AppendText("Q is not Prime!!!\n", Color.Red);
+                ++error;
+            }
+            if (p_value * q_value != n_value)
+            {
+                AppendText("p * q is not n!!!\n", Color.Red);
+                ++error;
+            }
+
+            if ((p_value - 1) * (q_value - 1) != pi_n_value)
+            {
+                AppendText("(p-1) * (q-1) is not 𝜑(n)!!!\n", Color.Red);
+                ++error;
+            }
+            if (GetGcd(e_value, pi_n_value) != 1)
+            {
+                AppendText("e and 𝜑(n) is not Relatively prime!!!\n", Color.Red);
+                ++error;
+            }
+            if ((e_value * d_value) % pi_n_value != 1)
+            {
+                AppendText("e * d % 𝜑(n) is not 1!!!\n", Color.Red);
+                ++error;
+            }
+            if (error != 0)
+            {
+                return;
+            }
+
+            //End checking error
+
+            //Encrypt String. (Character -> Decimal)
+            for (int i = 0; i < len; ++i)
+            {
+                cipher += GetPowResult(plain[i], e_value, n_value).ToString() + (i == len - 1 ? "" : " ");
+            }
+            Cipher_Textbox.Text = cipher;
+            AppendText("Encryption Complete!!!\n", Color.Black);
+
+            //Copy values to clipboard
+            System.Windows.Forms.Clipboard.SetText("Public Key : (" + n_value.ToString() + ", " + e_value.ToString() +
+                                                   ")      Private Key : (" + n_value.ToString() + ", " + d_value.ToString() + ")      Cipher : " + cipher);
+        }
+
+        private void Decrypt_Buttion_Click(object sender, EventArgs e)
+        {
+            //Basic Variable Declaration
+            ulong n_value = 0;
+            ulong d_value = 0;
+
+            String Cipher = Cipher_Textbox.Text;
+            String plain = "";
+            ulong temp = 0;
+
+            String[] Cipher_Array = Cipher.Split(' ');
+
+            int len = Cipher_Array.Length;
+            int error = 0;
+
+            Status_Textbox.Text = "";
+
+            //Start checking error
+
+            //Check if any values are empty
+            if (N_Textbox.Text == "")
+            {
+                AppendText("n value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (Decryption_Key_Textbox.Text == "")
+            {
+                AppendText("d value is empty!!!\n", Color.Red);
+                ++error;
+            }
+            if (Cipher_Textbox.Text == "")
+            {
+                AppendText("Cipher text is empty!!!", Color.Red);
+                ++error;
+            }
+
+            if (error != 0)
+            {
+                return;
+            }
+
+            //Check if values are not a number
+            try
+            {
+                n_value = Convert.ToUInt64(N_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("n value is not a number!!!\n", Color.Red);
+                return;
+            }
+            try
+            {
+                d_value = Convert.ToUInt64(Decryption_Key_Textbox.Text);
+            }
+            catch
+            {
+                AppendText("d value is not a number!!!\n", Color.Red);
+                return;
+            }
+
+            if (error != 0)
+            {
+                return;
+            }
+
+            //End checking error
+
+            //Decrypt String. (Number -> Character)
+            for (int i = 0; i < len; ++i)
+            {
+                temp = Convert.ToUInt64(Cipher_Array[i]);
+                plain += (char)GetPowResult(temp, d_value, n_value);
+
+            }
+            Plain_Textbox.Text = plain;
+
+            AppendText("Decryption Complete!!!\n", Color.Black);
+
+            //Copy plain text to clipboard
+            System.Windows.Forms.Clipboard.SetText("Plain : " + Plain_Textbox.Text);
+        }
+    }
+}
